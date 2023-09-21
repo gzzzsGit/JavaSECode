50dc9ce16164d953395e860b46e069175095a7ae
gzzz <1512008246@qq.com>
2023-09-22 00:00:14 +0800

    9.20

---
+++ Chapter10/src/com/atguigu05/communication/Exer1.java
@@ -0,0 +1,13 @@
+package com.atguigu05.communication;
+
+/**
+ * className: Exer1
+ * Package : com.atguigu05.communication
+ * Description:
+ *
+ * @Author gzzz
+ * @Create 2023/9/22 0:00
+ * @Version 1.0
+ */
+public class Exer1 {
+}
